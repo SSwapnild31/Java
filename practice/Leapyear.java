88b9764c6f4200ad70172e923c0c620089699a6f
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-06-16 22:12:15 +0530

    16-06-2025 Mon

---
+++ practice/Leapyear.java
@@ -0,0 +1,12 @@
+package codes;
+import java.util.*;
+
+class Leapyear 
+{
+	public static void main(String args[])
+	{
+		int year;
+		Scanner sc = new Scanner(System.in);
+		year = sc.nextInt();
+	}
+}
