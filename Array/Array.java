19fceb8e82ded1cec0e50605835938c76cb2d975
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-06-20 21:49:18 +0530

    20-06-2025 Fri

---
+++ Array/Array.java
@@ -0,0 +1,12 @@
+public class Array 
+{
+	public static void main(String args[])
+	{
+		int a[]= {0,1,2,3,4,5};
+		System.out.print("Array elements : ");
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.print(a[i]+" ");
+		}
+	}
+}
