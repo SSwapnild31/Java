0b4cfd6c2c4e145549c53995fc35d43b88c41c76
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-06-25 20:35:50 +0530

    25-06-2025 wed

---
+++ sorting/MergeSort.java
@@ -0,0 +1,14 @@
+package com.java.sorting;
+
+public class MergeSort
+{
+	public static void main(String[] args) 
+	{
+		int[] arr = {7,5,9,3,2,4,6};
+		
+		for(int ar : arr)
+		{
+			System.out.print(ar+" ");
+		}
+	}
+}
