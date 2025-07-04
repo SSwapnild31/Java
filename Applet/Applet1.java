0950722b353fcab23b9473ed3ab76c0de4f9b0dd
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-07-04 22:38:44 +0530

    04-07-2025 Fri

---
+++ Applet/Applet1.java
@@ -0,0 +1,15 @@
+import java.applet.*;
+import java.awt.*;
+
+/*
+<applet code = "Applet1" width=400 height=500>
+</applet>
+*/
+
+public class Applet1 extends Applet
+{
+	public void paint(Graphics g)
+	{
+		g.drawString("Swapnil Divate",25,50);
+	}
+}
