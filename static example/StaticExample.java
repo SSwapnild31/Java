0a3aec0354e51fb7bcd523450d9da3f914bbda9b
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-09-10 23:11:20 +0530

    10-09-2025 wed

---
+++ static example/StaticExample.java
@@ -0,0 +1,9 @@
+package com.staticexample.example;
+
+public class StaticExample {
+
+	public static void main(String[] args) {
+		//StaticExample2 s1 = new StaticExample2();
+		StaticExample2.add();
+	}
+}
