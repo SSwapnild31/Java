e928b851141bd44b85f439f08f1dfe1c821d73f2
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-09-11 22:55:21 +0530

    11-09-2025 Thu

---
+++ static example/ThisKeywordExampleMain.java
@@ -0,0 +1,11 @@
+package com.fuel.example;
+
+public class ThisKeywordExampleMain 
+{
+	public static void main(String[] args)
+	{
+		ThisKeywordExample tke = new ThisKeywordExample();
+		tke.getData(101, "mahi", "pune", 50225, "Swarajya", "Bharat");
+		tke.display();
+	}
+}
