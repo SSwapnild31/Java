a6af414b8d752793accb8db0e974f820c753bf78
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-09-13 22:49:33 +0530

    13-09-2025 sat

---
+++ static example/testpr.java
@@ -0,0 +1,19 @@
+
+//Even or odd
+
+class testpr
+{
+	public static void main(String args[]) 
+	{
+		int n;
+		n = Integer.parseInt(args[0]);
+		if(n%2==0)
+		{
+			System.out.println("Number is even");
+		}
+		else
+		{
+			System.out.println("Number is odd");
+		}
+	}
+}
