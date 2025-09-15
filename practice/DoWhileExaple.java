5b7ad3babb36830cb9e28974e46e9e72bcaa4e9c
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-09-15 22:44:16 +0530

    15-09-2025 Mon

---
+++ practice/DoWhileExaple.java
@@ -0,0 +1,14 @@
+package com.fuel.example;
+
+public class DoWhileExaple
+{
+	public static void main(String[] args)
+	{
+		int a = 0;
+		do {
+			System.out.println(a);
+			a++;
+		}while(11<10);
+	System.out.println("End of loop");
+	}
+}
