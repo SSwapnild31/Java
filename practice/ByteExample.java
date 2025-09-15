9d46b76b1095f1213e616418d7e20972af54bb8e
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-09-15 22:43:52 +0530

    15-09-2025 Mon

---
+++ practice/ByteExample.java
@@ -0,0 +1,22 @@
+package com.fuel.example;
+
+public class ByteExample 
+{
+	public static void main(String[] args)
+	{
+		byte b = -12;
+		char c = 'a';
+		int x = b;
+		float f = 15.9f;
+		byte z = (byte)f;
+		double d = f;
+		int a = (int)d;
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(x);
+		System.out.println(f);
+		System.out.println(z);
+		System.out.println(d);
+		System.out.println(a);
+	}
+}
