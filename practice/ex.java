caf820f79960265a1df9f79be0b8b41171dc4eab
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-10-09 23:47:43 +0530

    09-10-2025 Thu

---
+++ practice/ex.java
@@ -0,0 +1,17 @@
+/**
+ * @author Swapnil
+ *
+ */
+class ex
+{
+	public static void main(String args[])
+	{
+		for(int i=0;i<5;i++)
+		{
+			for(int j=i-1;j>5;j++)
+			{
+				System.out.println(j);
+			}
+		}
+	}
+}
