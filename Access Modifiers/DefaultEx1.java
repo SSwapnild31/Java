2a30fa51467691352ece76c8fc5875667c7a7c37
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-12-14 23:06:50 +0530

    14-12-2025 Son

---
+++ Access Modifiers/DefaultEx1.java
@@ -0,0 +1,8 @@
+package com.modifiers.example;
+
+public class DefaultEx1 {
+	int id;
+	void data() {
+		System.out.println(id);
+	}
+}
---
+++ Access Modifiers/DefaultMain.java
@@ -0,0 +1,12 @@
+package com.modifiers.example;
+
+
+public class DefaultMain {
+
+	public static void main(String[] args) {
+		//DefaultEx1 d1 = new DefaultEx1();
+		//d1.id=50;
+		//d1.data();
+	}
+
+}
